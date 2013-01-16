99dec2872c66ef43a214be25d1019088c73ef385
Kamil Wysocki <wysocki.km@gmail.com>
2013-01-16 16:25:18 +0100

    start betotrans.pl

---
+++ Betotrans/Betotrans/App_Start/BundleConfig.cs
@@ -0,0 +1,68 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Betotrans
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            RegisterBundlesScript(bundles);
+            RegisterBundlesStyle(bundles);
+        }
+        private static void RegisterBundlesScript(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                                    "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fancybox").Include(
+                        "~/Scripts/jquery.fancybox.pack.js",
+                        "~/Scripts/jquery.fancybox-buttons.js",
+                        "~/Scripts/jquery.fancybox-media.js",
+                        "~/Scripts/jquery.fancybox-thumbs.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+            "~/Scripts/knockout-*"));
+        }
+
+        private static void RegisterBundlesStyle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/style.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jquery-ui")
+                .Include("~/Content/themes/ui-lightness/jquery-ui.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fancybox/css")
+                .Include("~/Content/jquery.fancybox.css",
+                        "~/Content/jquery.fancybox-buttons.css",
+                        "~/Content/jquery.fancybox-thumbs.css"));
+        }
+    }
+}
